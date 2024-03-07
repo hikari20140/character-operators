99c19a63c0d2f87fd290b720d82436ece4072337
hikari20140 <160013755+hikari20140@users.noreply.github.com>
2024-03-07 16:23:50 +0900

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 using namespace std;
+//This function outputs variable "x" + variable "y"
 int plas(int x, int y){
     int num = x + y;
     return num;
 }
+//This function outputs variable "x" - variable "y"
 int minas(int x, int y){
     int num = x - y;
     return num;
+//This function outputs variable "x" * variable "y"
 }
 int puton(int x, int y){
     int num = x * y;
     return num;
 }
+//This function outputs variable "x" / variable "y"
 int divide(int x, int y){
     int num = x / y;
     return num;
 }
 int main(void){
-    // 
+    //How to use all functions:
     int plass = plas(10, 10);
     int minu = minas(11, 10);
     int put_on = puton(10, 10);
