a682e0d42062411b7cb98b4588beb39669c4ecb5
hikari20140 <160013755+hikari20140@users.noreply.github.com>
2024-03-09 08:23:38 +0900

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,17 @@
+def plas(x, y):
+    num = x + y;
+    return num
+def minus(x, y):
+    num = x - y;
+    return num
+def puton(x, y):
+    num = x * y
+    return num
+def divide(x, y):
+    num = x / y
+    return num
+    
+print(plas(10, 10))
+print(minus(20, 10))
+print(puton(10, 10))
+print(divide(100, 10))
